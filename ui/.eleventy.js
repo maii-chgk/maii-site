dd84a9289e50710c23f5fcf3bdef36af819fc0e1
Eugene Mirotin <emirotin@gmail.com>
2021-03-04 19:01:01 +0200

    News UI

--- ui/.eleventy.js
+++ ui/.eleventy.js
@@ -1,9 +1,26 @@
 const MarkdownIt = require("markdown-it");
+const formatDate = require("date-fns/format");
+const parseDate = require("date-fns/parse");
+const { ru } = require("date-fns/locale");
 
 module.exports = function (eleventyConfig) {
   const md = new MarkdownIt({
     html: true,
     typographer: true,
+  });
+
+  eleventyConfig.addFilter("json", (obj) => {
+    return JSON.stringify(obj);
+  });
+
+  eleventyConfig.addFilter("take", (arr, count) => {
+    return arr.slice(0, count);
+  });
+
+  eleventyConfig.addFilter("date", (date, format = "dd.MM.yyyy") => {
+    return formatDate(parseDate(date, "yyyy-MM-dd", new Date()), format, {
+      locale: ru,
+    });
   });
 
   eleventyConfig.addPairedShortcode("markdown", (content) => {
