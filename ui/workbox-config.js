3bc8a1efbbf9f14c6f1e873aa72979788a29a9ec
Eugene Mirotin <emirotin@gmail.com>
2021-03-05 14:27:01 +0200

    change workbox caching strategy

--- ui/workbox-config.js
+++ ui/workbox-config.js
@@ -1,5 +1,38 @@
 module.exports = {
   globDirectory: "_site/",
-  globPatterns: ["**/*.{html,png,ico,svg,css,js}"],
+  globPatterns: ["**/*.{html,png,ico,svg,jpg,jpeg,css,js}"],
   swDest: "_site/sw.js",
+
+  runtimeCaching: [
+    {
+      urlPattern: /\.html$/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "html",
+        expiration: {
+          maxEntries: 20,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:css|js)$/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "code",
+        expiration: {
+          maxEntries: 20,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|ico)$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "images",
+        expiration: {
+          maxEntries: 20,
+        },
+      },
+    },
+  ],
 };
