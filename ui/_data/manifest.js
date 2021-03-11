ef696e0bc6ae695e0cecc67b35b9581dd8ea4eb0
Eugene Mirotin <emirotin@gmail.com>
2021-03-11 22:07:54 +0200

    vision page

---
+++ ui/_data/manifest.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const { default: axios } = require("axios");
+
+const apiUrl = process.env.API_URL;
+
+module.exports = async () => {
+  try {
+    const res = await axios.get(`${apiUrl}/manifest`);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
