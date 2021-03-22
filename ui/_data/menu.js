6d51f2d15e1a1490421e4db6b44fcb5e30daf67c
Eugene Mirotin <emirotin@gmail.com>
2021-03-22 23:46:46 +0200

    publish vision

---
+++ ui/_data/menu.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: "Новости",
+    link: "/news",
+  },
+  {
+    title: "Обратная связь",
+    link: "/contact",
+  },
+  {
+    title: "Наше видение",
+    link: "/vision",
+  },
+];
