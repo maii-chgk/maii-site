e2bb69f90ff7c58f49241de7b872f1ddba64ac7c
Eugene Mirotin <emirotin@gmail.com>
2021-07-12 15:56:58 +0300

    social links in the side and footer menu

--- ui/_data/externalMenu.js
+++ ui/_data/externalMenu.js
@@ -2,31 +2,31 @@ module.exports = [
   {
     title: "Форум",
     icon: "",
-    url: "https://forum.znatoki.site/",
+    link: "https://forum.znatoki.site/",
   },
   {
     title: "Facebook",
-    icon: "",
-    url: "https://www.facebook.com/maii.info/",
+    icon: "facebook",
+    link: "https://www.facebook.com/maii.info/",
   },
   {
     title: "Telegram",
-    icon: "",
-    url: "https://t.me/maii_info",
+    icon: "telegram",
+    link: "https://t.me/maii_info",
   },
   {
     title: "VK",
-    icon: "",
-    url: "https://vk.com/maii_info",
+    icon: "vk",
+    link: "https://vk.com/maii_info",
   },
   {
     title: "Twitter",
-    icon: "",
-    url: "https://twitter.com/maii_info",
+    icon: "twitter",
+    link: "https://twitter.com/maii_info",
   },
   {
     title: "ЖЖ",
-    icon: "",
-    url: "https://maii-info.livejournal.com/",
+    icon: "livejournal",
+    link: "https://maii-info.livejournal.com/",
   },
 ];
