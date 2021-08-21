e9e0ef198044bb0bb351f95e334e462ea0cf2250
Dmitry Serzhenko <dmitry@serzhenko.me>
2021-08-21 08:26:43 +0300

    initial RSS/Atom commit

--- ui/.eleventy.js
+++ ui/.eleventy.js
@@ -71,3 +71,9 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(cacheBuster({}));
 };
+
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+};
