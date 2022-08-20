062ab68970b91286a7640aeaaf56013ea2ceab08
Jury Razumau <jury.razumau@zendesk.com>
2022-08-20 13:55:31 +0200

    increase page size for strapi news fetches

--- ui/_data/news.js
+++ ui/_data/news.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 const { default: axios } = require("axios");
 
 const apiUrl = process.env.API_URL;
+const apiPageSize = 500;
 
 module.exports = async () => {
   try {
-    const res = await axios.get(`${apiUrl}/news-items`);
+    const res = await axios.get(`${apiUrl}/news-items?_limit=${apiPageSize}`);
     return res.data;
   } catch (error) {
     console.error(error);
